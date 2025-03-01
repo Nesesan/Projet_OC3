75b92fda5b620daa7dc25e484a1c7c0757d69017
Nesesan <lounes.benz@outlook.fr>
2025-03-01 13:23:17 +0100

    project initialized

---
+++ src/main/java/com/nesesan/RentalOcApplication.java
@@ -0,0 +1,13 @@
+package com.nesesan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalOcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalOcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nesesan/RentalOcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesesan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalOcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
