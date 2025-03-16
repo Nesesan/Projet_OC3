a20b704ca27a9a597717bd1cc5ade85098a44d62
Nesesan <lounes.benz@outlook.fr>
2025-03-16 17:56:13 +0100

    Configure security : - previously added WebSecurityConfig (updated) - add JwtAuthenticationFilter (created)

--- src/main/java/com/nesesan/chatop/service/UserService.java
+++ src/main/java/com/nesesan/chatop/service/UserService.java
@@ -2,22 +2,37 @@ package com.nesesan.chatop.service;
 
 import com.nesesan.chatop.model.User;
 import com.nesesan.chatop.repository.UserRepository;
+import com.nesesan.chatop.security.JwtUtil;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,
+                                 PasswordEncoder passwordEncoder,
+                                 JwtUtil jwtUtil) {
+
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     public User getUserById(Integer id){
         return userRepository.findById(id).orElse(null);
     }
 
-    public User getUserByEmail(String email){
+    public User findByEmail(String email){
         return userRepository.findByEmail(email).orElse(null);
     }
 
@@ -33,6 +48,16 @@ public class UserService {
 
         userRepository.save(newUser);
         return "User registered successfully";
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByEmail(username);
+        if (optionalUser.isEmpty()) {
+            throw new UsernameNotFoundException("User not found" + username);
+        }
+        User user = optionalUser.get();
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
 }
 
